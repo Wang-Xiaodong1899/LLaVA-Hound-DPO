63c7f4a28ffea97697eb62216e62623d9169906b
Wang-Xiaodong1899 <1443472206@qq.com>
2025-02-24 22:22:46 +0800

    minor data

--- llava_hound_dpo/dpo_scripts/train_simpo_debug.sh
+++ llava_hound_dpo/dpo_scripts/train_simpo_debug.sh
@@ -20,7 +20,7 @@ output_dir=/volsparse3/wxd/ckpt/${WANDB_PROJECT}/${WANDB_NAME}
 mkdir -p $output_dir
 
 # DATA original data
-data_path=/workspace/wxd/LLaVA-Hound-DPO/llava_hound_dpo/self-gen/LLaVA-Hound-SFT_debate_aug_temp_top_p1.0_temp1.2.jsonl
+data_path=/workspace/wxd/LLaVA-Hound-DPO/llava_hound_dpo/self-gen/LLaVA-Hound-SFT_debate_temp0_aug_17k_top_p1.0_temp1.2.jsonl
 
 video_dir=/data/llava_hound/shareVideoGPTV/dpo_train_data
 image_dir="/"
