99659036941cd0c519576c5bed12e6703201101c
Xiaodong Wang <44627574+Wang-Xiaodong1899@users.noreply.github.com>
2025-02-25 16:05:26 +0800

    Update llava_arch.py

--- llava_hound_dpo/llava/model/llava_arch.py
+++ llava_hound_dpo/llava/model/llava_arch.py
@@ -133,11 +133,11 @@ class LlavaMetaForCausalLM(ABC):
 
     def encode_videos(self, videos):
         video_features = self.get_model().get_video_tower()(videos)
-        device = next(self.get_model().get_video_tower().parameters()).device
-        print(f"device: {device}")
-        self.get_model().mm_projector.to(device)
-        video_features = video_features.to(torch.bfloat16)
-        import pdb; pdb.set_trace()
+        # device = next(self.get_model().get_video_tower().parameters()).device
+        # print(f"device: {device}")
+        # self.get_model().mm_projector.to(device)
+        # video_features = video_features.to(torch.bfloat16)
+        # import pdb; pdb.set_trace()
         video_features = self.get_model().mm_projector(video_features)
         return video_features
 
